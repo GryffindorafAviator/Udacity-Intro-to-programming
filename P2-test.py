ecc7f907572d58f76f00271b69287a59d51d3a3a
Flash_W <phynew@hotmail.com>
2017-06-23 17:59:23 +0800

    Update P2-test.py

--- P2-test.py
+++ P2-test.py
@@ -3,39 +3,40 @@ blanksOfCharms = "Alohomora,Lumos,Wingardium Leviosa,Protegos,Legilimens,Accio F
 
 qusOfCharms = ["You took your wand out and said __1__.",
                "In the darkness, you said __2__ and the wand was lit up.",
-               "So you pointed it with your wand, saying __3__, and suddenly it floated up in the air. It was a flying broom.",
-               "All of a sudden, you heard someone saying a curse at you in the darkness, so you shouted __4__, and the curse was rebound.",
+               "So you pointed it with your wand, saying __3__ and suddenly it floated up in the air. It was a flying broom.",
+               "All of a sudden, you heard someone saying a curse at you in the darkness, so you shouted __4__ and the curse was rebound.",
                "The person fell down on the floor, so you approached him and accessed his mind by saying __5__.",
-               "So you said __6__ to summon your broom and said __7__ to extinguish the light on the wand."]
+               "So you said __6__ to summon your broom", "and said __7__ to extinguish the light on the wand."]
 
-ansOfcharmList = qusOfCharms[0].split()
-print ansOfcharmList
-print type(ansOfcharmList)
 
-blanksOfCharmsList = blanksOfCharms.split()
+blanksOfCharmsList = blanksOfCharms.split(',')
 print blanksOfCharmsList
 print type(blanksOfCharmsList)
 
 def word_in_pos(word, blanks):
-    for pos in word:
-        if pos in blanks:
+    for pos in blanks:
+        if pos in word:
             return pos
     return None
 
 
-def play_game(ml_string, ansList):
+def play_game(ml_string, blanks, replaceWords):
     replaced = []
     # ml_string = ml_string.split()
     for word in ml_string:
-        replacement = word_in_pos(word, ansList)
+        replacement = word_in_pos(word, blanks)
         if replacement != None:
-            replaced.append("corgi,")
+            replaced.append(replaceWords)
         else:
             replaced.append(word)
     replaced = " ".join(replaced)
     return replaced
 
+i = 0
 
 
 
-print play_game(ansOfcharmList, partsOfQusCharms) #ansList is __1__, ml_string is ansOfcharmList
+while i<len(partsOfQusCharms):
+    ansList = qusOfCharms[i].split()
+    print play_game(ansList,partsOfQusCharms[i], blanksOfCharmsList[i])
+    i += 1
