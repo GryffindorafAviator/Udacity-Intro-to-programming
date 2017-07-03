2e2136c426160c3e3faf32c4afde9c9ae7bebfea
Flash_W <phynew@hotmail.com>
2017-07-03 23:58:14 +0800

    Update media.py

--- media.py
+++ media.py
@@ -8,14 +8,28 @@
 
 import webbrowser
 
-class Movie():
-    # This class provides a way to store movie related information
-
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-        self.title = movie_title
-        self.storyline = movie_storyline
+class Video():
+    def __init__(self, title, storyline, poster_image, trailer_youtube):
+        self.title = title
+        self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+        
+
+class Movie():
+    # This class provides a way to store movie related information
+    
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
+    def __init__(self, duration, ratings):
+        self.duration = duration
+        self.ratings = ratings
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+        
+class TV_show:
+    def __init__(self, season, episode, TV_station):
+        self.season = season
+        self.episode = episode
+        self.TV_station = TV_station
